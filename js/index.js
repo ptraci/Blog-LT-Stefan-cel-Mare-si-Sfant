90a2d62152bd77726970a8af0f2abf02078dda80
ptraci <89113900+ptraci@users.noreply.github.com>
2022-05-25 20:18:04 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -42,7 +42,7 @@ function renderPosts(items) {
     for (let i = 0; i < items.length; i++) {
         let p = items[i];
         html += `             
-        <a class="postare" href="postare.html?id=${p.id}">  
+        <a class="postare" href="pages/postare.html?id=${p.id}">  
                            <img src="${p.data.img}">
                     <div class="postare-info">
                         <h4>${p.data.title}</h4>
